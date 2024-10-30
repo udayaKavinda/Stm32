eda0e8dff7470f7a60c27d373240f05a7ab11ccd
udayaKavinda <e19155@eng.pdn.ac.lk>
2024-10-30 16:34:50 +1100

    just added and multi sensor file corrected

---
+++ demo_project/Debug/Core/Src/acc_integration_log.d
@@ -0,0 +1,9 @@
+Core/Src/acc_integration_log.o: ../Core/Src/acc_integration_log.c \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_common.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration_log.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration.h
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_common.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration_log.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration.h:
---
+++ demo_project/Debug/Core/Src/example_service.d
@@ -0,0 +1,39 @@
+Core/Src/example_service.o: ../Core/Src/example_service.c \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_config.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_a121.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_common.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_a121.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_common.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_hal_definitions_a121.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_hal_integration_a121.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_processing.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_config_subsweep.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_config.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_rss_a121.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_hal_definitions_a121.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_sensor.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_detector_distance.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_detector_distance_definitions.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_processing.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_sensor.h \
+ C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_version.h
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_config.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_a121.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_common.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_a121.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_definitions_common.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_hal_definitions_a121.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_hal_integration_a121.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/integration/acc_integration.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_processing.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_config_subsweep.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_config.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_rss_a121.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_hal_definitions_a121.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_sensor.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_detector_distance.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_detector_distance_definitions.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_processing.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_sensor.h:
+C:/Users/udaya/OneDrive/Desktop/stm/demo_project/cortex_m4_gcc/rss/include/acc_version.h:
