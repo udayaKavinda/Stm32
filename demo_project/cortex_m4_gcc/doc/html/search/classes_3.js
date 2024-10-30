d5d8c30a12eb2c82ff2bdbb9a8d6ca0883a0b9f1
udayaKavinda <e19155@eng.pdn.ac.lk>
2024-10-30 15:52:11 +1100

    just added cortex m4

---
+++ demo_project/cortex_m4_gcc/doc/html/search/classes_3.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['hand_5fmotion_5fdetection_5falgo_5fconfig_5ft_1328',['hand_motion_detection_algo_config_t',['../structhand__motion__detection__algo__config__t.html',1,'']]],
+  ['hand_5fmotion_5fdetection_5falgo_5fresult_5ft_1329',['hand_motion_detection_algo_result_t',['../structhand__motion__detection__algo__result__t.html',1,'']]],
+  ['hand_5fmotion_5fdetection_5fconfig_5ft_1330',['hand_motion_detection_config_t',['../structhand__motion__detection__config__t.html',1,'']]],
+  ['hand_5fmotion_5fdetection_5fhandle_1331',['hand_motion_detection_handle',['../structhand__motion__detection__handle.html',1,'']]],
+  ['hand_5fmotion_5fdetection_5fresult_5ft_1332',['hand_motion_detection_result_t',['../structhand__motion__detection__result__t.html',1,'']]]
+];
